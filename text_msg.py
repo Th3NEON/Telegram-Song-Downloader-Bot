733679d467b7f46877869cb60c206c1c61e78042
Th3NEON <61600291+Th3NEON@users.noreply.github.com>
2022-08-24 01:49:23 +0500

    text_msg.py

--- text_msg.py
+++ text_msg.py
@@ -1,6 +1,6 @@
 import pyrogram
 
-START_TEXT_MSG = '🤖 Hello user!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\nto download some songs. 🎶\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
+START_TEXT_MSG = '🤖 Hello❤️❤️❤️❤️❤️❤️❤️!\n\n📩 I can download songs for you. Just send me the song name in below format:\n\n*/song*  _song name_  or\n*/song*  _musician name - song name_\n\nto download some songs. 🎶\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)'
 
 CONFIRMATION_TEXT_MSG = "✅ Song downloaded successfully!\n\n**■ ProTips**💡[ »Learn Bot](https://t.me/sanilaassistant_bot) \n                          [»Rate Bot](https://t.me/sanilaassistant_bot)\n                          [»Get help](https://t.me/sanilaassistant_bot)\n                          [»Give feedback](https://t.me/sanilaassistant_bot)"
 
